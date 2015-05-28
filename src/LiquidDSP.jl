a203a2b5181adcfe4700b66cdb76399867f0bf92
Jay Kickliter <jay@kickliter.com>
2015-05-28 10:21:20 -0600

    Transitioning to using bindeps for library loading

--- src/LiquidDSP.jl
+++ src/LiquidDSP.jl
@@ -1,20 +1,11 @@
 module LiquidDSP
 
-push!(Base.DL_LOAD_PATH, "/opt/local/lib")
+using BinDeps
+@BinDeps.load_dependencies
 
 const rrrf = ("rrrf", Float32,Float32,Float32)
 const crcf = ("crcf", Complex64,Float32,Complex64)
 const cccf = ("cccf", Complex64,Complex64,Complex64)
-
-@unix_only const libliquid = "libliquid"
-
-macro liquidcall(lqiuidf, returntype, argtypes, args...)
-    quote
-        ret = ccall(($liquidf, libliquid), $returntype, $argtypes, $(args...))
-    end
-end
-
-
 
 include("filter/filter.jl")
 
